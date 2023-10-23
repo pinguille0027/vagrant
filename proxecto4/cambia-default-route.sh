1618c5d1908b0943ed8d905cb5a2a527cefbc9b4
pinguille0027 <guillermo.orio27@gmail.com>
2023-10-23 12:50:55 +0200

    Changes to be committed: 	added: proxecto de tarefa entregable. Completado exercicio 1 (falta probar o punto 4)

---
+++ proxecto4/cambia-default-route.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+if ip r | grep default | grep 10.0.2.2 >/dev/null; then
+	echo "Cambiando a porta de enlace"
+	ip r del default via 10.0.2.2 dev eth0
+	ip r add default via 192.168.1.1 dev eth1 
+fi
+
+
