36ffe7b140a5230b94c832fdfd1d00600b3513f9
pinguille0027 <guillermo.orio27@gmail.com>
2023-11-07 13:27:31 +0100

    añadido: maquinas secundarias

--- proxecto4/instalacion-cacher.sh
+++ proxecto4/instalacion-cacher.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 export DEBIAN_FRONTEND=noninteractive
 apt update && apt upgrade
-apt install apt-cacher-ng -y && echo cacher instalado
+apt install apt-cacher-ng -y
+echo "PassThroughPattern : .*
+VfileUseRangeOps: -1" >> /etc/apt-cacher-ng/acng.conf
+echo "AdminAuth:guille:renaido" > /etc/apt-cacher-ng/security.conf
+systemctl restart apt-cacher-ng
+echo "cacher instalado"
