e528742914f20f78d0fad943ceb2bd5733012b0a
pinguille0027 <guillermo.orio27@gmail.com>
2023-11-28 09:12:29 +0100

    Configuración de apache usando interfaces virtuales y varios dominios

--- redes escenario 1/instalacion-dns.sh
+++ redes escenario 1/instalacion-dns.sh
@@ -3,11 +3,9 @@ apt install bind9 -y
 echo "named.conf"
 cat /vagrant/named.conf.options > /etc/bind/named.conf.options
 echo "conf.local"
-echo 'zone "gofasir.com" {
-    type master;
-    file "/etc/bind/gofasir.com";
-};' > /etc/bind/named.conf.local
+cat /vagrant/named.conf.local > /etc/bind/named.conf.local
 echo "zona"
 cat /vagrant/zona > /etc/bind/gofasir.com
+cat /vagrant/zonatienda1 > /etc/bind/tienda1.com
 systemctl restart bind9
 nslookup www.gofasir.com
