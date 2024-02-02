97ae4cdf5fb1f9f4771103e3e650c04fe921f5ac
pinguille0027 <guillermo.orio27@gmail.com>
2024-02-02 13:07:26 +0100

    practicas 1 y 2 de apache

---
+++ redes escenario 2/virtual.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /home
+mkdir virtual
+echo "<h1>Practica 2</h1><h2>GOF</h2>" > virtual/p2.1.html
+cd virtual/
+mkdir puente
+echo "<h1>me voy de puente</h1><h2>GOF</h2>" > puente/puente.html
